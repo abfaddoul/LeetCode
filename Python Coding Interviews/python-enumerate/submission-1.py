e7933caad6611aa0371d1cf17474bbdd20562e68
The Qubit <147248837+abfaddoul@users.noreply.github.com>
2026-09-08 10:16:50 +0100

    Add: python-enumerate - submission-1

---
+++ Python Coding Interviews/python-enumerate/submission-1.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+def add_index(numbers: List[int]) -> List[int]:
+    res = []
+    for i, number in enumerate(numbers):
+        res.append(i + number)
+    return res
+
+
+def find_word(words: List[str], target: str) -> int:
+    for i, word in enumerate(words):
+        if word == target:
+            return i
+    return -1
+
+
+# do not modify below this line
+print(add_index([5, 5, 5]))
+print(add_index([10, 20, 30, 40]))
+
+print(find_word(["apple", "banana", "orange"], "banana"))
+print(find_word(["cat", "dog", "bird"], "bird"))
+print(find_word(["red", "green", "blue"], "yellow"))
