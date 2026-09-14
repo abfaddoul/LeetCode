1b15db302a043ad8143bb3616ada3dc1d85f3d2d
The Qubit <147248837+abfaddoul@users.noreply.github.com>
2026-09-14 10:37:38 +0100

    Add: python-hash-map-basics - submission-0

---
+++ Python Coding Interviews/python-hash-map-basics/submission-0.py
@@ -0,0 +1,29 @@
+def create_dictionary(name: str, age: int) -> dict[str, int]:
+    info = {
+        name : age
+    }
+    return info
+
+
+def get_age(ages: dict[str, int], name: str) -> int:
+    return ages[name]
+
+
+def add_or_update(
+    ages: dict[str, int],
+    name: str,
+    age: int
+) -> dict[str, int]:
+    ages[name] = age
+    return ages
+
+
+# do not modify below this line
+print(create_dictionary("Alice", 25))
+print(create_dictionary("Bob", 30))
+
+print(get_age({"Alice": 25, "Bob": 30}, "Alice"))
+print(get_age({"Alice": 25, "Bob": 30}, "Bob"))
+
+print(add_or_update({"Alice": 25}, "Bob", 30))
+print(add_or_update({"Alice": 25, "Bob": 30}, "Alice", 26))
