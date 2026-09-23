53632eb36e3505bf4f5cd0698e6f788fc61f3e8f
The Qubit <147248837+abfaddoul@users.noreply.github.com>
2026-09-23 10:37:13 +0100

    Add: majority-element-ii - submission-3

---
+++ Data Structures & Algorithms/majority-element-ii/submission-3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        dct, res, n = defaultdict(int), [], len(nums)
+
+        for num in nums:
+            dct[num] += 1
+        for key, value in dct.items():
+            if value > n / 3:
+                res.append(key)
+        return res        
