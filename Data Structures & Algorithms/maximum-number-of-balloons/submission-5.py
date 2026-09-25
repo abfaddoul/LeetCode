9bc7920b98b05d6f1ad3d39a3bff1eb1316b4857
The Qubit <147248837+abfaddoul@users.noreply.github.com>
2026-09-25 18:58:40 +0100

    Add: maximum-number-of-balloons - submission-5

---
+++ Data Structures & Algorithms/maximum-number-of-balloons/submission-5.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        count = Counter(text)
+        balloon = Counter("balloon")
+        res = len(text)
+        for c in balloon:
+            res = min(res, count[c] // balloon[c])
+        return res
