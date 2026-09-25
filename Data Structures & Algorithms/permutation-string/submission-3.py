ef8bfb66036c999247a5c24ba24f694edc6d92e3
The Qubit <147248837+abfaddoul@users.noreply.github.com>
2026-09-25 08:35:39 +0100

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        i, n = 0, len(s1) - 1
+        while i + n < len(s2):
+            if sorted(s1) == sorted(s2[i:i+n+1]):
+                return True
+            i += 1
+        return False
